eb5ef8d015bed1d0b2466722ab4eb4890e16cc96
Jayromberg <jayro.dev@gmail.com>
2023-04-25 10:00:31 -0300

    feat: Gets e Sets da classe movie

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ import domain.Movie;
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
+
+        myMovie.name = "O poderoso chefão";
+
     }
 }
--- src/domain/Movie.java
+++ src/domain/Movie.java
@@ -1,10 +1,58 @@
 package domain;
 
 public class Movie {
-    String name;
-    int releaseYear;
-    double assessments;
-    int ratingTotals;
-    int durationInMinutes;
-    boolean includedInThePlan;
+    private String name;
+    private int releaseYear;
+    private double assessments;
+    private int ratingTotals;
+    private int durationInMinutes;
+    private boolean includedInThePlan;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setAssessments(double assessments) {
+        this.assessments = assessments;
+    }
+
+    public double getAssessments() {
+        return assessments;
+    }
+
+    public void setRatingTotals(int ratingTotals) {
+        this.ratingTotals = ratingTotals;
+    }
+
+    public int getRatingTotals() {
+        return ratingTotals;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setIncludedInThePlan(boolean includedInThePlan) {
+        this.includedInThePlan = includedInThePlan;
+    }
+
+    public boolean isIncludedInThePlan() {
+        return includedInThePlan;
+    }
 }
